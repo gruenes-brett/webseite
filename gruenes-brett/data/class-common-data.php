30c0dcb8600350fb505ef85fe18a351ca3901f07
Joerg <jeremyjove@gmail.com>
2021-05-27 22:46:37 +0200

    #24 changed API

--- gruenes-brett/data/class-common-data.php
+++ gruenes-brett/data/class-common-data.php
@@ -34,10 +34,8 @@ class Common_Data {
         $calendar_name = '';
         $latest_date   = null;
         $start_date    = static::get_earliest_display_date();
-        $is_admin      = Comcal_User_Capabilities::administer_events();
 
         $events_iterator = new Comcal_Event_Iterator(
-            ! $is_admin,
             $category,
             $calendar_name,
             $start_date ? $start_date->get_date_str() : null,
