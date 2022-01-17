15111db821351d3414fadf0a1f4f4d00ee161190
joergrs <joergschroeter84@gmail.com>
2022-01-17 22:15:58 +0100

    #45 collect access statistics

---
+++ ansible/roles/goaccess_stats/templates/run_goaccess.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Analyze the log for the current month (or yesterday's month if today is the 1st)
+#
+# This script copies yesterday's apache logfile to a monthly cache folder that contains all.
+# While copying, the log file is already filtered for the requested domain.
+# Afterwards, goaccess is called for all the logs of the month and a static html file
+# is created.
+
+DOMAIN={{ domain }}
+
+LOGDIR=/var/log/apache2
+LOGFILE_BASE=$LOGDIR/other_vhosts_access.log
+OUTDIR_BASE={{ stats_base_dir }}/$DOMAIN
+
+# consider yesterday's logs
+YEAR=$(date --date="yesterday" +%Y)
+MONTH=$(date --date="yesterday" +%m)
+DAY=$(date --date="yesterday" +%d)
+
+HTML_OUT_DIR=$OUTDIR_BASE/$YEAR
+LOG_CACHE_DIR={{ log_cache_dir }}/$DOMAIN/$YEAR/$MONTH
+
+mkdir -p $HTML_OUT_DIR
+mkdir -p $LOG_CACHE_DIR
+
+# Copy yesterdays log file to cache dir
+cat $LOGFILE_BASE.1 | grep "^$DOMAIN" > $LOG_CACHE_DIR/access_$DAY.log
+
+# Create HTML for given month
+zcat --force $LOG_CACHE_DIR/*.log | goaccess --output=$HTML_OUT_DIR/$MONTH.html --log-format=VCOMBINED -
