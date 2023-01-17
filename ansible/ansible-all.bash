845d2cd27d90a6db6bec83427c3c13f51dcf00bf
joergrs <joergschroeter84@gmail.com>
2023-01-17 22:07:26 +0100

    unecessary file

--- ansible/ansible-all.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i gbtesting_host $1 && \
-    # ansible-playbook -i dresden_host $1 && \
-    ansible-playbook -i chemnitz_host $1 && \
-    ansible-playbook -i vogtland_host $1 && \
-    ansible-playbook -i leipzig_host $1
