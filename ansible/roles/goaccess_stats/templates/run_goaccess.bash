d00a8eba8cbd4a3af24828aab1608fc9f4a3b768
joergrs <joergschroeter84@gmail.com>
2024-03-13 22:17:38 +0100

    make goaccess stats script runnable in analysis only mode

--- ansible/roles/goaccess_stats/templates/run_goaccess.bash
+++ ansible/roles/goaccess_stats/templates/run_goaccess.bash
@@ -6,16 +6,19 @@
 # Afterwards, goaccess is called for all the logs of the month and a static html file
 # is created.
 
+# Optionally provide parameters YEAR and MONTH to re-run analysis for the given month
+# Example: ./run_goaccess.bash 2024 03
+
 DOMAIN={{ domain }}
 
 LOGDIR=/var/log/apache2
 LOGFILE_BASE=$LOGDIR/other_vhosts_access.log
 OUTDIR_BASE={{ stats_base_dir }}/$DOMAIN
 
-# consider yesterday's logs
-YEAR=$(date --date="yesterday" +%Y)
-MONTH=$(date --date="yesterday" +%m)
-DAY=$(date --date="yesterday" +%d)
+# consider yesterday's logs (if YEAR and MONTH are not given by parameter)
+YEAR=${1:-$(date --date="yesterday" +%Y)}
+MONTH=${2:-$(date --date="yesterday" +%m)}
+DAY=${3:-$(date --date="yesterday" +%d)}
 
 HTML_OUT_DIR=$OUTDIR_BASE/$YEAR
 LOG_CACHE_DIR={{ log_cache_dir }}/$DOMAIN/$YEAR/$MONTH
@@ -23,8 +26,13 @@ LOG_CACHE_DIR={{ log_cache_dir }}/$DOMAIN/$YEAR/$MONTH
 mkdir -p $HTML_OUT_DIR
 mkdir -p $LOG_CACHE_DIR
 
-# Copy yesterdays log file to cache dir
-cat $LOGFILE_BASE.1 | grep "^$DOMAIN" > $LOG_CACHE_DIR/access_$DAY.log
+YESTERDAY=$(date --date="yesterday" +%Y-%m-%d)
+if [ "$YEAR-$MONTH-$DAY" = "$YESTERDAY" ]; then
+    echo "Copy yesterdays log file from $YESTERDAY to cache dir $LOG_CACHE_DIR"
+    cat $LOGFILE_BASE.1 | grep "^$DOMAIN" > $LOG_CACHE_DIR/access_$DAY.log
+else
+    echo "Running in analysis only mode for $YEAR-$MONTH"
+fi
 
 # Additional expressions for lines that should be ignored in the log file
 FILTER_EXPRESSIONS='wp-cron\.php|wp-login\.php|ical\/all|ical\/ev|xmlrpc\.php|wp-json|wp-content|wp-includes|wp-admin|wp-sitemap'
